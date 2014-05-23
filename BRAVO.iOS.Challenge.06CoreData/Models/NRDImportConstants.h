f7302a9b74c8dfec346b87e4e17deea7d22f388b
Mike Dockerty <mdockert@nerdery.com>
2014-05-23 14:28:45 -0500

    Added JSON import keys

--- BRAVO.iOS.Challenge.06CoreData/Models/NRDImportConstants.h
+++ BRAVO.iOS.Challenge.06CoreData/Models/NRDImportConstants.h
@@ -9,20 +9,20 @@
 #ifndef BRAVO_iOS_Challenge_06CoreData_NRDImportConstants_h
 #define BRAVO_iOS_Challenge_06CoreData_NRDImportConstants_h
 
-#define kNRDFirstNameKey       'firstName'
-#define kNRDLastNameKey        'lastName'
-#define kNRDHireDateKey        'hireDate'
-#define kNRDIdentiferKey       'identifier'
-#define kNRDManagerIdKey       'managerId'
-#define kNRDDirectReportIdsKey 'directReportIds'
-#define kNRDMentorIdKey        'mentorId'
-#define kNRDMenteeIdsKey       'menteeIds'
+#define kNRDFirstNameKey       @"firstName"
+#define kNRDLastNameKey        @"lastName"
+#define kNRDHireDateKey        @"hireDate"
+#define kNRDIdentiferKey       @"identifier"
+#define kNRDManagerIdKey       @"managerId"
+#define kNRDDirectReportIdsKey @"directReportIds"
+#define kNRDMentorIdKey        @"mentorId"
+#define kNRDMenteeIdsKey       @"menteeIds"
 
-#define kNRDPBRAdminKey        'admin'
-#define kNRDPBRClientKey       'client'
-#define kNRDPBRZeroKey         'zero'
-#define kNRDPBRBravoKey        'bravo'
-#define kNRDPBRDateKey         'pbrDate'
-#define kNRDPBREmployeeId      'employeeId'
+#define kNRDPBRAdminKey        @"admin"
+#define kNRDPBRClientKey       @"client"
+#define kNRDPBRZeroKey         @"zero"
+#define kNRDPBRBravoKey        @"bravo"
+#define kNRDPBRDateKey         @"pbrDate"
+#define kNRDPBREmployeeId      @"employeeId"
 
 #endif
