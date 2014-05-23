b5fbec0a9365da03a068b272d6625f66f65687f1
Mike Dockerty <mdockert@nerdery.com>
2014-05-23 14:20:12 -0500

    Added a clearDatabase method to the core data manager

--- BRAVO.iOS.Challenge.06CoreData/Managers/AppCoreDataManager.m
+++ BRAVO.iOS.Challenge.06CoreData/Managers/AppCoreDataManager.m
@@ -28,4 +28,23 @@
     return [applicationSupportDirectory URLByAppendingPathComponent:@"BRAVO_iOS_Challenge_06CoreData" isDirectory:YES];
 }
 
++ (void)clearDatabase
+{
+    NSError * error;
+    // retrieve the store URL
+    NSURL * storeURL = [[self.managedObjectContext persistentStoreCoordinator] URLForPersistentStore:[[[self.managedObjectContext persistentStoreCoordinator] persistentStores] lastObject]];
+    // lock the current context
+    [self.managedObjectContext lock];
+    [self.managedObjectContext reset];//to drop pending changes
+    //delete the store from the current managedObjectContext
+    if ([[self.managedObjectContext persistentStoreCoordinator] removePersistentStore:[[[self.managedObjectContext persistentStoreCoordinator] persistentStores] lastObject] error:&error])
+    {
+        // remove the file containing the data
+        [[NSFileManager defaultManager] removeItemAtURL:storeURL error:&error];
+        //recreate the store like in the  appDelegate method
+        [[self.managedObjectContext persistentStoreCoordinator] addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error];//recreates the persistent store
+    }
+    [self.managedObjectContext unlock];
+}
+
 @end
